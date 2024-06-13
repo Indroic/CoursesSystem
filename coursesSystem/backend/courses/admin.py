2b87596b4d15f33c578d42eb4c12331793cc0639
indroic <davidlatosefkindriago@gmail.com>
2024-06-12 22:24:01 -0400

    feat(coursesSystem/backend): añadido de barra de busqueda

--- coursesSystem/backend/courses/admin.py
+++ coursesSystem/backend/courses/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
 from .models import Course, Lesson
 
+class CoursesAdmin(admin.ModelAdmin):
+    model = Course
+    search_fields = ('name', 'uploaded_by__username')
+    list_display = ('name',  'level', 'miniature', 'num_lessons', 'uploaded_by')
 
+    fieldsets = (
+        ("Details", {'fields': ('name', 'description', 'level', 'miniature', 'uploaded_by') } ),)
+    
+    list_filter = ('level', 'uploaded_by')
 
 class LessonAdmin(admin.ModelAdmin):
     model = Lesson
-    list_display = ('title', 'description', 'created_at', 'course')
+    list_display = ('title', 'created_at', 'course')
+    search_fields = ('title', 'course__name', 'course__uploaded_by__username')
 
     fieldsets = (
         ("Details", {'fields': ('title', 'description', 'miniature', 'video', 'course') } ),
@@ -19,15 +28,7 @@ class LessonAdmin(admin.ModelAdmin):
             obj.course.save()
 
         return super().save_model(request, obj, form, change)
-class CoursesAdmin(admin.ModelAdmin):
-    model = Course
 
-    list_display = ('name', 'description', 'level', 'miniature', 'num_lessons', 'uploaded_by')
-
-    fieldsets = (
-        ("Details", {'fields': ('name', 'description', 'level', 'miniature', 'uploaded_by') } ),)
-    
-    list_filter = ('level', 'uploaded_by')
     
 
 
