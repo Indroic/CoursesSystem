3e65ff24ec14d1042b847fc612ea90f7cbd475e6
indroic <davidlatosefkindriago@gmail.com>
2024-06-13 19:57:18 -0400

    feat(coursesSystem/backend): añadido de certificados y examenes realizados al panel de admin

--- coursesSystem/backend/certificados/admin.py
+++ coursesSystem/backend/certificados/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Certificate, ExamRealized
 
-# Register your models here.
+class CertificateAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'course', 'examrealized', 'date')
+    list_display_links = ('id', 'user', 'course', 'examrealized', 'date')
+    list_filter = ('user', 'course', 'examrealized', 'date')
+    search_fields = ('user__username', 'course__name', "id", "date")
+
+
+class ExamRealizedAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'exam', 'date')
+    list_display_links = ('id', 'user', 'exam', 'date')
+    list_filter = ('user', 'exam', 'date')
+    search_fields = ('user__username', 'exam__title', "id", "date")
+
+
+
+admin.site.register(Certificate, CertificateAdmin)
+admin.site.register(ExamRealized, ExamRealizedAdmin)
+
--- coursesSystem/backend/certificados/models.py
+++ coursesSystem/backend/certificados/models.py
@@ -6,8 +6,8 @@ from courses.models import Course
 # Create your models here.
 
 
-class ExamRealizado(models.Model):
-    id = models.UUIDField(primary_key=True, editable=False)
+class ExamRealized(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,editable=False)
     
     exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
 
@@ -16,14 +16,21 @@ class ExamRealizado(models.Model):
     date = models.DateTimeField(auto_now=True, editable=False)
 
 
-class Certificado(models.Model):
-    id  = models.UUIDField(primary_key=True, editable=False)
+    def __str__(self):
+        return f"{self.user} - {self.exam} - {self.date}"
+
+
+class Certificate(models.Model):
+    id  = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
-    examrealizado = models.ForeignKey(ExamRealizado, on_delete=models.CASCADE)
+    examrealized = models.ForeignKey(ExamRealized, on_delete=models.CASCADE)
 
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
     date = models.DateTimeField( auto_now=True, editable=False)
 
+
+    def __str__(self):
+        return f"{self.user} - {self.course} - {self.date}"
