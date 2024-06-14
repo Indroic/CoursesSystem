c85d8bebbbfc4e355d3e2a027201b2fbfe8eae08
indroic <davidlatosefkindriago@gmail.com>
2024-06-14 18:37:00 -0400

    docs(coursesSystem/backend): documentacion de modulo de cursos

--- coursesSystem/backend/courses/admin.py
+++ coursesSystem/backend/courses/admin.py
@@ -3,27 +3,46 @@ from django.utils.translation import gettext_lazy as _
 from .models import Course, Lesson
 
 class CoursesAdmin(admin.ModelAdmin):
+    """ Modelo de Administracion de Cursos """
+
+    # Modelo de Cursos
     model = Course
+
+    # campos de Cursos al buscar
     search_fields = ('name', 'uploaded_by__username')
+
+    # campos de Cursos al mostrar en el listado
     list_display = ('name',  'level', 'miniature', 'num_lessons', 'uploaded_by')
 
+    # Orden de Cursos en el listado
     fieldsets = (
         (_("Details"), {'fields': ('name', 'description', 'level', 'miniature', 'uploaded_by') } ),)
     
+    # filtros de Cursos
     list_filter = ('level', 'uploaded_by')
 
 class LessonAdmin(admin.ModelAdmin):
+    # modelos de Lecciones
     model = Lesson
+
+    # campos de Lecciones al mostrar en el listado
     list_display = ('title', 'created_at', 'course')
+
+    # campos de Lecciones al buscar
     search_fields = ('title', 'course__name', 'course__uploaded_by__username')
 
+    # Orden de Lecciones en el listado
     fieldsets = (
         (_("Details"), {'fields': ('title', 'description', 'miniature', 'video', 'course') } ),
     )
 
+    # filtros de Lecciones
     list_filter = ('updated_at', 'created_at', 'course')
 
+    # Metodos
     def save_model(self, request, obj: Lesson, form, change):
+
+        # aumenta el numero de lecciones siempre y cuando no sea un cambio
         if not change:
             obj.course.num_lessons += + 1
             obj.course.save()
@@ -32,6 +51,7 @@ class LessonAdmin(admin.ModelAdmin):
 
     
 
+# Registra los modelos de administracion
 
 admin.site.register(Course, CoursesAdmin)
 admin.site.register(Lesson, LessonAdmin)
--- coursesSystem/backend/courses/models.py
+++ coursesSystem/backend/courses/models.py
@@ -43,26 +43,26 @@ def generar_nombre_video(instance, filename):
 # Create your models here.
 class Course(models.Model):
     
-    
+    # Crea un identificador unico
     id = models.UUIDField(primary_key=True, 
                           default=uuid.uuid4, 
                           editable=False)
     
-    
+    # Define el nombre del curso
     name = models.CharField(max_length=200, 
                             unique=True, 
                             null=False, 
                             blank=False,
                             verbose_name=_('Name of Course'))
     
-    
+    # Define la descripccion del curso
     description = models.CharField(max_length=10000,
                                    unique=False, 
                                    null=False, 
                                    blank=False,
                                    verbose_name=_('Course Description'))
     
-    
+    # Define el nivel del curso
     level = models.CharField(max_length=20, 
                              unique=False, 
                              null=False, 
@@ -73,131 +73,143 @@ class Course(models.Model):
                                       (_("Advanced"), _("Advanced"))),
                              verbose_name=_('Course Level'))
     
-    
+    # Define el autor
     uploaded_by = models.ForeignKey(settings.AUTH_USER_MODEL, 
                                     on_delete=models.CASCADE, 
                                     related_name='courses',
                                     verbose_name=_('Uploaded by'))
     
-    
+    # Define la miniatura
     miniature = models.ImageField(upload_to=generar_nombre_miniatura, 
                                   blank=False, 
                                   null=False, 
                                   default="miniatures/default.jpg",
                                   verbose_name=_('Miniature'))
     
-    
+    # Define el numero de lecciones
     num_lessons = models.IntegerField(default=0,
                                       editable=False,
                                       verbose_name=_('Number of Lessons'))
     
-    
+    # Define la fecha de creacion
     created_at = models.DateTimeField(auto_now_add=True,
                                       verbose_name=_('Created at'))
     
-    
+    # Define la fecha de actualizacion
     updated_at = models.DateTimeField(auto_now=True,
                                       verbose_name=_('Updated at'))
 
 
     class Meta:
+        """ Metadatos de clase """
+
+        # Nombre de la tabla
         verbose_name = _('Course')
+
+        # Nombre de la tabla plural
         verbose_name_plural = _('Courses')
 
 
     def __str__(self):
         return self.name + " - " + self.uploaded_by.username
     
+    # Funcion de eliminacion
     def delete(self, *args, **kwargs):
+        # Verifica que la miniatura no sea la por defecto
         if self.miniature.path != settings.DEFAULT_MINIATURE:
             try:
+                # Elimina la miniatura
                 os.remove(self.miniature.path)
             except FileNotFoundError:
                 pass
-
+        # Retorna la funcion anterior
         return super().delete(*args, **kwargs)
 
 
 
 class Lesson(models.Model):
     
-    
+    # Crea un identificador unico
     id = models.UUIDField(primary_key=True, 
                           default=uuid.uuid4, 
                           editable=False)
     
-    
+    # Define el titulo
     title = models.CharField(max_length=500,
                              unique=True, 
                              null=False, 
                              blank=False,
                              verbose_name=_('Title'))
     
-    
+    # Define la descripccion
     description = models.CharField(max_length=10000,
                                    unique=False, 
                                    null=False, 
                                    blank=False,
                                    verbose_name=_('Description'))
     
-    
+    # Define el curso
     course = models.ForeignKey(Course, 
                                on_delete=models.CASCADE, 
                                related_name='Lessons',
                                verbose_name=_('Course'))
     
-    
+    # Define la miniatura
     miniature = models.ImageField(upload_to=generar_nombre_miniatura, 
                                   blank=False, 
                                   null=False, 
                                   default="miniatures/default.jpg",
                                   verbose_name=_('Miniature'))
 
-
+    # Define el video
     video = models.FileField(upload_to=generar_nombre_video, 
                              blank=True, 
                              null=True,
                              verbose_name=_('Video'))
 
-
+    # Define la fecha de creacion
     created_at = models.DateTimeField(auto_now_add=True,
                                       verbose_name=_('Created at'))
     
-    
+    # Define la fecha de actualizacion
     updated_at = models.DateTimeField(auto_now=True,
                                       verbose_name=_('Updated at'))
     
     class Meta:
+        """ Metadatos de clase """
+
+        # Nombre de la tabla
         verbose_name = _('Lesson')
+
+        # Nombre de la tabla plural
         verbose_name_plural = _('Lessons')
 
     def __str__(self):
         return self.title + " - " + self.course.name + " - " + self.course.uploaded_by.username
 
 
+    # Funcion de eliminacion
     def delete(self, *args, **kwargs):
-
+        # Verifica que la miniatura no sea la por defecto
         if self.miniature.path != settings.DEFAULT_MINIATURE:
             try:
+                # Elimina la miniatura
                 os.remove(self.miniature.path)
             except FileNotFoundError:
                 pass
 
         try:
+            # Elimina el video
             os.remove(self.video.path)
         except Exception as e:
             pass
 
 
-
-        if self.course.num_lessons <= 0:
-            return super().delete(*args, **kwargs)
+        # Disminuye el numero de lecciones en el curso siempre y cuando el numero de lecciones sea mayor a 0
+        if self.course.num_lessons > 0:
+            self.course.num_lessons -= 1
         
-        self.course.num_lessons -= 1
-        
-        self.course.save()
-        
-        
+            self.course.save()    
 
         return super().delete(*args, **kwargs)
-    
+        
